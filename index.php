9a1290cc3b8c44ec2a1d87a36875ff7663f95764
Michael Ludvig <mludvig@logix.net.nz>
2017-01-24 16:40:56 +1300

    Improved index.php

--- index.php
+++ index.php
@@ -1,32 +1,52 @@
+<?php
+# By Michael Ludvig - https://aws.nz
+
+$expire = "1 minute";
+
+date_default_timezone_set("UTC");
+require '/var/www/html/aws.phar';
+include 'config.php';
+?>
 <html>
 <head>
 <title>Pre-signed URL demo</title>
 </head>
 <body>
+<h1>AWS S3 Pre-signed URL demo</h1>
+<p>Upload files and images to S3 Bucket <b><a href="https://console.aws.amazon.com/s3/home?region=<?=$region?>&bucket=<?=$bucket?>&prefix=" target="_blank"><?=$bucket?></a></b> to test the signed and unsigned access.</p>
+<table border='1'>
+<tr><td><b>Object</b></td><td><b>Unsigned URL</b></td><td><b>Signed URL</b></td></tr>
 <?php
-require '/var/www/html/aws.phar';
-
-$object = "image.jpg";
-$expire = "1 minute";
-
-include 'config.php';
-
 $s3 = new Aws\S3\S3Client([
     'version' => '2006-03-01',
     'region'  => $region,
 ]);
 
-$cmd = $s3->getCommand('GetObject', [
-    'Bucket' => $bucket,
-    'Key'    => $object,
-]);
+$bucket_url = "https://s3-{$region}.amazonaws.com/{$bucket}";
 
-$request = $s3->createPresignedRequest($cmd, "+{$expire}");
-$signed_url = (string) $request->getUri();
+$iterator = $s3->getIterator('ListObjects', array('Bucket' => $bucket));
 
-$base_url = "https://s3-{$region}.amazonaws.com/{$bucket}/{$object}";
-echo "<p>Direct URL (returns error):<br/><a href=\"{$base_url}\">{$base_url}</a></p>";
-echo "<p>Signed URL expires in <i>{$expire}</i>, reload page to refresh URL:<br/><a href=\"{$signed_url}\">{$signed_url}</a></p>";
+foreach ($iterator as $object) {
+    $key = $object['Key'];
+    $cmd = $s3->getCommand('GetObject', [
+        'Bucket' => $bucket,
+        'Key'    => $key,
+    ]);
+
+    $request = $s3->createPresignedRequest($cmd, "+{$expire}");
+    $signed_url = (string) $request->getUri();
+
+    echo("<tr><td>$key</td><td><a href=\"{$bucket_url}/{$key}\">Direct</a></td><td><a href=\"{$signed_url}\">Expires in $expire</a></td></tr>");
+}
 ?>
+</table>
+<p>
+<ul>
+<li><i>The unsigned URLs may return an error depending on the ACL of the file.</i></li>
+<li><i>The signed URLs expire in <b><?=$expire ?></b>. Reload the page to refresh the URLs.</i></li>
+</ul>
+</p>
+<p>Source code is available here: <a href="https://github.com/mludvig/aws-s3sign-demo">https://github.com/mludvig/aws-s3sign-demo</a>.</p>
+<address>By <a href="https://aws.nz/">Michael Ludvig</a></address>
 </body>
 </html>
